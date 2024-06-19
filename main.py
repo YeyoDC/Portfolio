72b0d446f1f81234df6bfe428b005795bb91ef52
Diego Cuellar <dcuellarhernandez01@mynbcc.ca>
2024-06-19 14:57:20 -0300

    added image and title with text

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit as st
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/DiegoPhoto.jpg")
 with col2:
     st.title("Diego Cuellar")
     content = """
     Hi, I am Diego, I am a python student, I am trying to learn python
      and you will see all my applications on here
     """
-    st.write(content)
+    st.info(content)
