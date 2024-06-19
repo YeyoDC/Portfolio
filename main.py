8b8b4990ab9934d79fedb3ebdc34566760e417e0
Diego Cuellar <dcuellarhernandez01@mynbcc.ca>
2024-06-19 16:10:07 -0300

    Added description, images, url and empty space

--- main.py
+++ main.py
@@ -18,11 +18,19 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 # writing the content outside of the column
 st.write(content2)
 # adding new columns
-col3, col4 = st.columns(2)
+col3, empty_colum, col4 = st.columns([1.5, 0.5, 1.5])
+
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Code](https://pythonhow.com)")
 with col4:
     for index, row in df[11:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Code](https://pythonhow.com)")
+
