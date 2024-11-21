67c08384fd0f2f97a7ab729917c6e833ac8230c6
diego <diego_cuellar24@hotmail.com>
2024-11-21 17:12:38 -0400

    added email class

--- Home.py
+++ Home.py
@@ -56,3 +56,34 @@ with skill_col_3:
         list += "<li>" + skill + "</li>"
     st.html(list)
 
+
+st.subheader("Projects:")
+content2 = """
+Below you can find some of my projects from school that I have been working on
+"""
+
+# writing the content outside of the column
+st.write(content2)
+# adding new columns
+col3, middle_column, col4 = st.columns([1.5, 0.5, 1.5])
+with col3:
+    st.header("To do app")
+    st.write("App that allows you to add activities so you remember, and once done they can be deleted")
+    st.image("images/1.png")
+    st.write("[Source Code](https://github.com/YeyoDC/to-do-WebApp)")
+
+    st.header("Social Media Website")
+    st.write("Social media website, that allows you to post what you think in a fun way")
+    st.image("images/15.png")
+    st.write("[Source Code](https://github.com/YeyoDC/y-socialMedia)")
+
+with col4:
+    st.header("Students Scores")
+    st.write("App that allows you upload students scores, coded in c# using Object-oriented principles")
+    st.image("images/12.png")
+    st.write("[Source Code](https://github.com/YeyoDC/StudentsScore)")
+
+    st.header("Pet Modeling Photograph")
+    st.write("Website dedicated to attract pets to serve as models for different projects like commercials and movies")
+    st.image("images/2.png")
+    st.write("[Source Code](https://github.com/YeyoDC/pet_modeling)")
